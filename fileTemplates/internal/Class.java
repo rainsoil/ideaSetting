aa6aee8aeae5ebe5baeb25a7f7a4f90e2dad0b3b
'luyanan' <'luyanan0718@163.com'>
2021-01-17 21:39:47 +0800

    IU-2019.3.5 Create baseRefactoring.xml, markdown.xml, ide.general.xml, studioFlags.xml, project.default.xml, laf.xml, lombok-plugin.xml, Class.java, jdk.table.xml, Default.xml, applicationLibraries.xml, path.macros.xml, file.template.settings.xml, ignore.xml, wsl.distributions.xml, filetypes.xml, colors.scheme.xml, KeyPromoterXSettings.xml, mybatis.xml, editor.xml, mavenVersion.xml, debugger.xml, updates.xml

---
+++ fileTemplates/internal/Class.java
@@ -0,0 +1,10 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+/**
+ * <p>${desp}</p>
+ * @author luyanan
+ * @since ${DATE}
+ * 
+ **/
+public class ${NAME} {
+}
